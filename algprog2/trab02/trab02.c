a0f8485c8b775c4416f61db9180b0063d54b00d8
Kadu Terra <116027962+kKadoca@users.noreply.github.com>
2022-11-22 10:57:25 -0300

    Trab2 - Update

--- algprog2/trab02/trab02.c
+++ algprog2/trab02/trab02.c
@@ -14,15 +14,15 @@
 // catalogando tipos de dados
 typedef enum {
     NUM,       // 0
-    ADD = '+', // 43
-    SUB = '-', // 45
-    MUL = '*', // 42
-    DIV = '/', // 47
-    EXP = '^', // 94
-    LOG = 'L', // 76
-    FAT = '!', // 33
-    MOD = '%', // 37
-    FIM = '@'  // 64
+    ADD = '+', // 43 
+    SUB = '-', // 45 
+    MUL = '*', // 42 
+    DIV = '/', // 47 
+    EXP = '^', // 94 
+    LOG = 'L', // 76 
+    FAT = '!', // 33 
+    MOD = '%', // 37 
+    FIM = '@'  // 64 
 } tipo_op;
 
 // estrutura de uma celula
@@ -33,18 +33,26 @@ typedef struct cel {
 } celula;
 
 // funcoes de fila
-void enfileira_enc_C(celula **f, double y, tipo_op o);
-double desenfileira_enc_C(celula *i, celula **f, int t);
+void enfileira_enc_C(celula **f, double y, int o);
+void desenfileira_enc_C_R(celula *i, celula **f, celula *t);
+
 // funcoes de pilha
 void empilha_enc_C(double y, celula *t);
+double desempilha_enc_C(celula *t);
+double desempilhaCalculando_C(celula *t, int o);
+
+// funcoes matematicas
+int piso(double n);
+double logbase2(double n);
+double fatorial(int n);
+double exponenciacao(double n1, double n2);
 
 
 int main() {
-    int j, k = 1;
-    //scanf("%d", &k);
+    int j, k;
+    scanf("%d", &k);
 
     for (j = 0; j < k; j++) {
-        char val[MAX+1];
 
         // inicializa-se uma fila com cabeça
         celula *i, *f;
@@ -53,83 +61,52 @@ int main() {
         i->prox = NULL;
         f = i;
 
+        // inicializa-se uma pilha com cabeça       
+        celula *t;
+        t = (celula *) malloc(sizeof (celula));
+        t->prox = NULL;
+
         // enfileira toda a linha de valores e operadores
+        char val[MAX+1];
         do {
             scanf("%s", &val);
             if (strlen(val) > 1) {
                 // se for um valor numerico
                 enfileira_enc_C(ff, atof(val), NUM);
-                //printf("numero\n");
             } else {
                 // se for um operador
                 enfileira_enc_C(ff, val[0], val[0]);
-                //printf("operador\n");
             }
             
         } while (val[0] != FIM);
-        //printf("TUDO FOI ENFILEIRADO!\n");
 
-        /*// percorre a fila imprimindo PARA TESTAR!!
-        int w;
-        do {
-            w = desenfileira_enc_C(i, ff);
-            printf("%d ", w);
-        } while (w != INT_MIN);
-        */
+        // desenfileira os valores  
+        celula *q;
+        q = i->prox;
 
-        // inicializa-se uma pilha com cabeça       
-        celula *t;
-        t = (celula *) malloc(sizeof (celula));
-        t->prox = NULL;
-        // inicializa-se variaveis para calculos
-        int tipo;
-        double n;
-        
-        n = desenfileira_enc_C(i, ff, tipo);
-        while (tipo != FIM) {
-            switch (tipo) {
-            case NUM:
-                empilha_enc_C(n, t);
-                break;
-            case ADD:
-                /* code */
-                break;
-            case SUB:
-                /* code */
-                break;
-            case MUL:
-                /* code */
-                break;
-            case DIV:
-                /* code */
-                break;
-            case EXP:
-                /* code */
-                break;
-            case LOG:
-                /* code */
-                break;
-            case FAT:
-                /* code */
-                break;
-            case MOD:
-                /* code */
-                break;
-            
-            default:
-                printf("Erro: tipo não definido");
-                break;
+        if (q != NULL) {
+            while (q->tipo != FIM) {
+                desenfileira_enc_C_R(i, ff, t);
+                // se numero - empilha
+                // se operador - calcula
+                q = i->prox;   
             }
         }
-        
-        
-    
+
+        // imprime a pilha
+        double w = 0;
+        while (w != INT_MIN) {
+            w = desempilha_enc_C(t);
+            if (w != INT_MIN) {
+                printf("%.2f\n", w);
+            }
+        }
     }
     return 0;
 }
 
 // funcoes de fila
-void enfileira_enc_C(celula **f, double y, tipo_op o) {
+void enfileira_enc_C(celula **f, double y, int o) {
     celula *nova;
     nova = (celula *) malloc(sizeof (celula));
     nova->valor = y;
@@ -171,25 +148,28 @@ void enfileira_enc_C(celula **f, double y, tipo_op o) {
     *f = nova;
 }
 
-
-double desenfileira_enc_C(celula *i, celula **f, int t) {
-    double x;
+void desenfileira_enc_C_R(celula *i, celula **f, celula *t) {
+    double x, n;
+    int o;
     celula *p;
-    x = INT_MIN;
     p = i->prox;
-    t = p->tipo;
-    
+    o = p->tipo;
+
     if (p != NULL) {
-        x = p->valor;
+        if (o == NUM) {
+            x = p->valor;
+            empilha_enc_C(x, t);
+        } else {
+            n = desempilhaCalculando_C(t, o);
+            empilha_enc_C(n, t);
+        }
+        
         i->prox = p->prox;
         free(p);
         if (i->prox == NULL) {
             *f = i;
         }
-    } else {
-        printf("FIM DA FILA!\n");
     }
-    return x;
 }
 
 // funcoes de pilha
@@ -202,27 +182,127 @@ void empilha_enc_C(double y, celula *t) {
 }
 
 double desempilha_enc_C(celula *t) {
-    double x;
+    double z;
     celula *p;
-
+    
     if (t->prox != NULL) {
         p = t->prox;
-        x = p->valor;
+        z = p->valor;
         t->prox = p->prox;
         free(p);
-        return x;
+        return z;
     }
     else {
-        printf("Pilha vazia!\n");
         return INT_MIN;
-    }  
+    }
 }
 
-double soma_pilha_C_R(double n, celula *t){
-    while (t->prox != NULL) {
-        n = 0;
-        return soma_pilha_C_R(t->valor, t); 
+double desempilhaCalculando_C(celula *t, int o){
+    double res, n1, n2;
+
+    switch (o) {
+    case ADD:
+        /* soma */
+        n1 = desempilha_enc_C(t);
+        n2 = desempilha_enc_C(t);
+        res = n2 + n1;
+        break;
+    case SUB:
+        /* subtracao */
+        n1 = desempilha_enc_C(t);
+        n2 = desempilha_enc_C(t);
+        res = n2 - n1;
+        break;
+    case MUL:
+        /* multiplicacao */
+        n1 = desempilha_enc_C(t);
+        n2 = desempilha_enc_C(t);
+        res = n2 * n1;
+        break;
+    case DIV:
+        /* divisao */
+        n1 = desempilha_enc_C(t);
+        n2 = desempilha_enc_C(t);
+        res = n2 / n1;
+        break;
+    case EXP:
+        /* exponenciacao inteira */
+        n1 = desempilha_enc_C(t);
+        n2 = desempilha_enc_C(t);
+        res = exponenciacao(n2, n1);
+        break;
+    case LOG:
+        /* piso do log2 */
+        n1 = desempilha_enc_C(t);
+        res = piso(logbase2(n1));
+        break;
+    case FAT:
+        /* fatorial do piso */
+        n1 = desempilha_enc_C(t);
+        res = fatorial(piso(n1));
+        break;
+    case MOD:
+        /* modulo do piso */
+        n1 = desempilha_enc_C(t);
+        n2 = desempilha_enc_C(t);
+        res = piso(n2) % piso(n1);
+        break;
+    default:
+        break;
     }
-    
-        // return n + soma(n - 1);
+
+    return res;
 }
+
+// funcoes matematicas
+int piso(double n) {
+  if (n >= 0)
+    return (int) n;
+  else
+    return (int)(n + ((int)(-n)+1)) - ((int)(-n)+1);
+}
+
+double exponenciacao(double n1, double exp) {
+    double r = n1;
+
+    if (exp == 0) {    // expoente 0
+        return 1;
+    }
+    if (exp > 0) {    // expoente positivo
+        for (int i = 2; i <= exp ; i++) {
+            r *= n1;
+        }
+    } else {        // expoente negativo
+        for (int i = -2; i >= exp; i--) {
+            r *= n1;
+        }
+        r = 1 / r;
+    }
+
+    return r;
+}
+
+double logbase2(double n) {
+    int r = 0;
+
+    while (n > 1) {
+        n /= 2;
+        r += 1;
+    }
+    return r; 
+}
+
+double fatorial(int n) {
+    int exp = n - 1;
+
+    if (n != 0) {
+        for (int i = exp; i > 1; i--) {
+            n *= exp;
+            exp--;
+        }
+    }
+
+    return n;
+}
+
+
